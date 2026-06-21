64a8a5d924a79c210c681a6501d3e0a9efd45f94
GarciaElkin <garciaelkin56@gmail.com>
2026-06-21 16:29:17 -0500

    actualizado

--- GestionEstadio/Organizador.java
+++ GestionEstadio/Organizador.java
@@ -1,5 +1,47 @@
 package GestionEstadio;
 
-public class Organizador {
-    //hola
+public class Organizador extends Usuario {
+    //Se definen sus atributos propias
+    private String empresa;
+    private String cargo;
+
+    //Se define sus constructor
+    public Organizador(String codigoUnico, String cedula, String nombre, String apellido, String usuario,
+        String contraseña, String correo, Rol rol) {
+        super(codigoUnico, cedula, nombre, apellido, usuario, contraseña, correo, rol);
+        this.empresa=empresa;
+        this.cargo=cargo;
+    }
+
+    @Override
+    public void consularEntrada(){
+        System.out.println();
+    }
+
+    public void generarReporteDeVentas(){
+
+    }
+
+    public void salir(){
+
+    }
+    //Metodos Getters y Setters
+    public String getempresa() {
+        return empresa;
+    }
+
+    public void setempresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
 }
+    
+
