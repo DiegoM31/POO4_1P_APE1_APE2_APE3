7a9e813afffbf3251c66c97eb2ea77f10d69c810
Diego Jahir Maldonado Zalamea <diemaldo@espol.edu.ec>
2026-07-08 17:55:24 -0500

    avanzando el metodo iniciarSecion de Sistema

---
+++ GestionEstadio/Archivos.java
@@ -0,0 +1,132 @@
+package GestionEstadio;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class Archivos {
+
+        
+    public static ArrayList<String> cargarUsuarios(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo);
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }   
+
+    public static ArrayList<String> cargarPartidos(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo);
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }   
+    public static ArrayList<String> cargarKits(String nombrearchivo) {
+        ArrayList<String> lineas = new ArrayList<>();
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            archivo = new File(nombrearchivo);
+            fr = new FileReader(archivo,StandardCharsets.UTF_8);
+            br = new BufferedReader(fr);
+
+            // Lectura del fichero
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                lineas.add(linea);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta 
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return lineas;
+
+    }            
+}
+   
--- GestionEstadio/Sistema.java
+++ GestionEstadio/Sistema.java
@@ -1,20 +1,65 @@
 package GestionEstadio;
 
-import java.util.List;
+
+
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Sistema {
-    private  List<Usuario> usuarios;
-    private List<Partido> partidos;
-    private List<Kit> kits;
-    private List<Compra> compras;
-    
-    public static void main(){
+    private  ArrayList<Usuario> usuarios;
+    private ArrayList<Partido> partidos;
+    private ArrayList<Kit> kits;
+    private ArrayList<Compra> compras;
+    private Scanner sc;
+
+
+    public Sistema() {
+        // Inicializamos las listas vacías de forma segura sin ignorar parámetros
+        this.usuarios = new ArrayList<>();
+        this.partidos = new ArrayList<>();
+        this.kits = new ArrayList<>();
+        this.compras = new ArrayList<>();
+        this.sc = new Scanner(System.in);
+        cargarUsuariosDesdeArchivo();
     }
 
-    public void iniciarSesion(){  
-
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.iniciarSecion();
     }
+
+
+
+
+    public void iniciarSecion() {
+        System.out.println("\n\n\n\n\n");
+        System.out.println("---- Iniciar Sesión --");
+        System.out.println("Ingrese su usuario:");
+        String usuarioI = sc.nextLine();
+        System.out.println("Ingrese su contraseña:");
+        String contraseñaI = sc.nextLine();
+        boolean usuarioEncontrado = false;
+        for (Usuario u : this.usuarios) {
+            // Aquí es donde verificamos si el usuario y la contraseña coinciden
+            // Usamos los métodos getUsuario() y getContraseña() de la clase Usuario
+            if (u.getUsuario().equals(usuarioI) && u.getContraseña().equals(contraseñaI)) {
+                usuarioEncontrado = true;
+                System.out.println("¡Inicio de sesión exitoso!");
+                System.out.println("Bienvenido/a " + u.getNombre() + " " + u.getApellido());
+                break; // Salimos del método si encontramos al usuario
+            } 
+        }
+        if(!usuarioEncontrado) {
+            System.out.println("Usuario o contraseña incorrectos. Intente nuevamente.");
+        }
+    }
+
+
+
+
+
+
+
 
     public void notificar( Aficionado aficionado, Compra compra){
 
@@ -25,4 +70,52 @@ public class Sistema {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void cargarUsuariosDesdeArchivo() {
+        // Llamamos al método estático que ya creaste en tu clase Archivos
+        ArrayList<String> lineas = Archivos.cargarUsuarios("usuarios.txt");
+
+        // Recorremos las líneas. Empezamos en i = 1 para SALTAR la cabecera (CodigoUnico|Cedula...)
+        for (int i = 1; i < lineas.size(); i++) {
+            String linea = lineas.get(i);
+            
+            // Cortamos la línea usando el split
+            String[] datos = linea.split("\\|");
+
+            // Guardamos cada posición en variables (¡Tus índices calculados!)
+            String codigoUnico = datos[0];
+            String cedula = datos[1];
+            String nombre = datos[2];
+            String apellido = datos[3];
+            String usuario = datos[4];       // Índice 4
+            String contraseña = datos[5];       // Índice 5
+            String correo = datos[6];
+            Rol rol = Rol.valueOf(datos[7]);  
+        
+        Usuario usuario2 = new Usuario(codigoUnico, cedula, nombre, apellido, usuario, contraseña, correo, rol);
+        this.usuarios.add(usuario2);      // Índice 7
+        }
+    }
 }
