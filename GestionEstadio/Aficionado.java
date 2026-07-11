5c01ad756967f5b57edaaae62145a66551fa84a6
GarciaElkin <garciaelkin56@gmail.com>
2026-07-10 19:37:59 -0500

    Futuros cambios por clase Compra y Kit

--- GestionEstadio/Aficionado.java
+++ GestionEstadio/Aficionado.java
@@ -1,4 +1,6 @@
 package GestionEstadio;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Aficionado extends Usuario {
     private String celular;
@@ -16,8 +18,6 @@ public class Aficionado extends Usuario {
         this.paisFavorito = paisFavorito;
     }
 
-
-
     @Override
     public void mostrarMenu() {
         System.out.println("Menú de Aficionado: \r\n" + //
@@ -29,8 +29,91 @@ public class Aficionado extends Usuario {
             "Seleccione una opción: ");
     }
 
+    public void consultarPartidos(ArrayList<Partido> listaPartidos){
+        System.out.println("Partidos encontrados:\n");
+        int contador=1;
 
-    public void setCelular(String celular){
+        for(Partido p: listaPartidos){
+            System.out.println(contador + ". Código: " + p.getCodigo());
+            System.out.println("Partido: " + p.getSeleccionLocal() + " vs " + p.getSeleleccionVisitante());
+            System.out.println("Fecha: " + p.getFecha());
+            System.out.println("Estadio: " + p.GetEstadio());
+            System.out.println("Ciudad: " + p.getCiudad());
+            System.out.println("Fase: ");
+
+            System.out.println("Zonas disponibles:");
+            // System.out.println("- GENERAL      | Disponibles: " + p.getEntradasGeneral() + " | Precio: $" + /* precio aquí */);
+            // System.out.println("- PREFERENCIAL | Disponibles: " + p.getEntradasPreferencial() + " | Precio: $" + /* precio aquí */);
+            // System.out.println("- VIP          | Disponibles: " + p.getentradasVIP() + " | Precio: $" + /* precio aquí */);
+            System.out.println("--------------------------------------------------");
+            contador++;
+        }
+    }
+
+    public void comprarEntrada(){
+
+        // System.out.println("Ingrese el código del partido:");
+        // String codPartido = sc.nextLine();
+        // Partido p = buscarPartido(listaPartidos, codPartido); 
+        // if (p == null) {
+        //     System.out.println("Partido no encontrado.");
+        //     return;
+        // }
+        // System.out.println("Elija la zona (a. GENERAL, b. PREFERENCIAL, c. VIP):");
+        // String opcionZona = sc.nextLine();
+
+        // System.out.println("Ingrese la cantidad de entradas:");
+        // int cantidad = Integer.parseInt(sc.nextLine());
+        
+        // double total = cantidad * precioSeleccionado;
+        // System.out.println("Total a pagar: $" + total);
+        // System.out.println("Ingrese número de tarjeta:");
+        // String tarjeta = sc.nextLine();
+
+        // System.out.println("Pago exitoso. Gracias por su compra.");
+
+        // Compra nuevaCompra = new Compra("ENTRADA", p.getCodigo(), new Date(), cantidad, total, this.getCodigoUnico());
+        
+        // p.setEntradasGeneral(p.getEntradasGeneral() - cantidad);
+    }
+
+    public void comprar(Partido partido,String zona,int cantidad){
+        
+    }
+
+    public void comprar(Kit kit, int candidad){
+
+        // if (kit.getDisponibles() > 0) {
+        //     System.out.println("Incluye: " + kit.getPartidos());
+        //     System.out.println("Precio total: $" + kit.getPrecio());
+        //     Scanner sc = new Scanner(System.in);
+        //     System.out.println("Ingrese número de tarjeta:");
+        //     String tarjeta = sc.nextLine();
+        // } else {
+        //     System.out.println("Kit no disponible.");
+        // }
+    }
+
+    @Override
+    public void consultarEntrada(){
+
+    // boolean tieneCompras = false;
+    // for (Compra c : listaTotal) {
+    //     if (c.getCodigoAficionado().equals(this.codigoUnico)) { 
+    //             c.getCodigoCompra(), 
+    //             c.getTipo(), 
+    //             c.getFechaCompra(), 
+    //             c.getCodigoReferencia(), 
+    //             c.getValorPagado());
+    //         tieneCompras = true;
+    //     }
+    //     
+    //} if (!tieneCompras) {
+    //     System.out.println("No registra compras realizadas hasta el momento.");
+    }
+
+    //Metodos Getters y Setters
+     public void setCelular(String celular){
         this.celular= celular;
     }
     public String getCelular(){
@@ -44,18 +127,4 @@ public class Aficionado extends Usuario {
         return this.paisFavorito;
     }
 
-
-
-    @Override
-    public void consultarEntrada(){
-        System.out.println();
-    }
-
-    public void comprar(Partido partido){
-
-    }
-
-    public void comprar(Kit kit){
-        
-    }
 }
