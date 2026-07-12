ea473cc492a139d4b757d902af3e8d63c33c55cb
CarlosVilla07 <eduavill@espol.edu.ec>
2026-07-11 21:04:37 -0500

    Add files via upload

--- GestionEstadio/Compra.java
+++ GestionEstadio/Compra.java
@@ -2,6 +2,95 @@ package GestionEstadio;
 
 
 public class Compra {
+    private static int contadorCompras=0;
+    private String codigoCompra;
+    private String tipo;
+    private String codigoReferencia;
+    private Date fechacompra;
+    private int cantidad;
+    private double valorPagado;
+    private String codigoAficionado;
+    public Compra(String tipo, String codigoReferencia,Date fechacompra,int cantidad,double valorPagado,String codigoAficionado){
+        this.codigoCompra="C"+String.format("%03d", ++contadorCompras);
+        this.tipo=tipo;
+        this.codigoReferencia=codigoReferencia;
+        this.fechacompra=fechacompra;
+        this.cantidad=cantidad;
+        this.valorPagado=valorPagado;
+        this.codigoAficionado=codigoAficionado;
+
+    }
+    public Compra(String codigoCompra, String tipo, String codigoReferencia, Date fechacompra, int cantidad, double valorPagado, String codigoAficionado) {
+        this.codigoCompra = codigoCompra;
+        this.tipo = tipo;
+        this.codigoReferencia = codigoReferencia;
+        this.fechacompra = fechacompra;
+        this.cantidad = cantidad;
+        this.valorPagado = valorPagado;
+        this.codigoAficionado = codigoAficionado;
+        String soloNumeros = codigoCompra.substring(1);
+        int numeroActual = Integer.parseInt(soloNumeros);
+        if (numeroActual > contadorCompras) {
+            contadorCompras = numeroActual;
+        }
+    }
+    public String getCodigoCompra() {
+        return codigoCompra;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getCodigoReferencia() {
+        return codigoReferencia;
+    }
+    public void setCodigoReferencia(String codigoReferencia) {
+        this.codigoReferencia = codigoReferencia;
+    }
+    public Date getFechacompra() {
+        return fechacompra;
+    }
+    public void setFechacompra(Date fechacompra) {
+        this.fechacompra = fechacompra;
+    }
+    public int getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    public double getValorPagado() {
+        return valorPagado;
+    }
+    public void setValorPagado(double valorPagado) {
+        this.valorPagado = valorPagado;
+    }
+    public String getCodigoAficionado(){
+        return codigoAficionado;
+    }
+    public void setCodigoAficionado(String codigoAficionado){
+        this.codigoAficionado=codigoAficionado;
+    }
+    public String formatToLineaArchivo() {
+        SimpleDateFormat sdf = new SimpleDateFormat("Dia/Mes/Anio");
+        String fechaFormateada = sdf.format(this.fechacompra);
+        return this.codigoCompra + "|" + this.tipo + "|" + this.codigoReferencia + "|" + 
+               fechaFormateada + "|" + this.cantidad + "|" + this.valorPagado + "|" + this.codigoAficionado;
+    }
+    @Override
+    public String toString(){
+        SimpleDateFormat sdf = new SimpleDateFormat("Dia/Mes/Anio HH:mm");
+        return "--------------------"+"\n"+
+        "Compra: "+ codigoCompra+" ["+tipo+"] "+"\n"+
+        "Ref: "+codigoReferencia+"\n"+
+        "Fecha de compra: "+sdf.format(fechacompra)+"\n"+
+        "Cantidad: "+cantidad+"\n"+
+        "Total: $"+valorPagado+"\n"+
+        "Codigo Aficionado: "+codigoAficionado+"\n"+
+        "--------------------" ;    
+    }
 
     
 }
--- GestionEstadio/Kit.java
+++ GestionEstadio/Kit.java
@@ -1,5 +1,82 @@
 package GestionEstadio;
 
 public class Kit {
+    private String codigo;
+    private String nombre;
+    private String descripcion;
+    private ArrayList<String> partidosIncluidos;
+    private double precio;
+    private int disponibles;
+    public Kit(){
+    }
+    public Kit(String codigo, String nombre, String descripcion, ArrayList<String> partidosIncluidos, double precio, int disponibles) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.partidosIncluidos = partidosIncluidos;
+        this.precio = precio;
+        this.disponibles = disponibles;
+
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public ArrayList<String> getPartidosIncluidos() {
+        return partidosIncluidos;
+    }
+    public void setPartidosIncluidos(ArrayList<String> partidosIncluidos) {
+        this.partidosIncluidos = partidosIncluidos;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public int getDisponibles() {
+        return disponibles;
+    }
+    public void setDisponibles(int disponibles) {
+        this.disponibles = disponibles;
+    }
+    public String getPartidosComoString() {
+        return String.join(",", this.partidosIncluidos);
+    }
+    public String formatToLineaArchivo() {
+        return this.codigo + "|" + this.nombre + "|" + this.descripcion + "|" + 
+               getPartidosComoString() + "|" + this.precio + "|" + this.disponibles;
+    }
+    @Override
+    public String toString(){
+        return "-------------------"+
+        "Kit: "+nombre+"\n"+
+        "Descripcion: " + descripcion + "\n" +
+        " Precio: "+precio+"\n"+
+        " Disponibles: "+disponibles+"\n"+
+        "Partidos incluidos: " + partidosIncluidos+"\n"+
+        "-------------------";
+
+    }
+
+
     
 }
+
+
+
